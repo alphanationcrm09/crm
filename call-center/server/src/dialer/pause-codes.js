f3aae53f02e06d5e7c291c7291c8d8585b06f04a
Aalphanation <mrskhan202502@gmail.com>
2026-08-26 23:57:54 +0500

    Add standard agent pause codes

---
+++ call-center/server/src/dialer/pause-codes.js
@@ -0,0 +1,8 @@
+export const PAUSE_CODES = Object.freeze([
+  { code: 'break', label: 'Break' },
+  { code: 'lunch', label: 'Lunch' },
+  { code: 'meeting', label: 'Meeting' },
+  { code: 'training', label: 'Training' },
+  { code: 'personal', label: 'Personal' },
+  { code: 'technical', label: 'Technical Issue' }
+]);
